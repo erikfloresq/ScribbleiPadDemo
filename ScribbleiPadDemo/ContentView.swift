398eaa5f4b2b87044f85bc3b2160abcc194262ea
Erik Flores <erikfloresq@icloud.com>
2021-08-26 07:50:41 -0300

    Initial Commit

---
+++ ScribbleiPadDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScribbleiPadDemo
+//
+//  Created by Erik Flores on 26/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScribbleiPadDemo/ScribbleiPadDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribbleiPadDemoApp.swift
+//  ScribbleiPadDemo
+//
+//  Created by Erik Flores on 26/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ScribbleiPadDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
